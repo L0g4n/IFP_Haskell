e206e82e2a1b37bde6140713a134cb3e39d53cd5
L0g4n <yannik.klubertanz@posteo.de>
2017-11-17 16:12:00 +0100

    Add solution for exercise 2.2 "Pythagorean Triples".

--- sheet2/main.hs
+++ sheet2/main.hs
@@ -9,3 +9,9 @@ listEvens x y
     | y < x = []
     | otherwise = [y | y <- [y, y - 1 .. x], y `mod` 2 == 0]
 
+type Triple = (Int, Int, Int)
+
+pythagoreanTriples :: Int -> [Triple]
+pythagoreanTriples n 
+    | n < 0 = []
+    | otherwise = [(a, b, c) | a <- [1 .. n], b <- [a .. n], c <- [b .. n], a^2 + b^2 == c^2]
