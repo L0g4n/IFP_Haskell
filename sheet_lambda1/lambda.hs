282b63a1be1cb72f8d371f0be1667948a10984fa
L0g4n <yannik.klubertanz@posteo.de>
2018-02-02 23:53:08 +0100

    Add (indexed) variables and fancy printing of those.

--- sheet_lambda1/lambda.hs
+++ sheet_lambda1/lambda.hs
@@ -1,3 +1,12 @@
 -- Provides a basic implementation of the Lambda Calculus
 module Lambda where
 
+-- Data type for indexed variables, e.g. "x1, x2, ..."
+data LamVariable = Name String | Idx Int String
+
+-- instance for show to provide "fancy printing" of variables
+instance Show LamVariable where
+    show (Name x) = x
+    show (Idx i x)
+        | i == 0 = x -- Index 0 is always omitted
+        | otherwise = x ++ show i
